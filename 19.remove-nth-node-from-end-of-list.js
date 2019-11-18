15739d99fd68b4f3714d5a8f164558bf79edee3c
yihong <zouzou0208@gmail.com>
2019-11-18 21:16:20 +0800

    day6

--- 19.remove-nth-node-from-end-of-list.js
+++ 19.remove-nth-node-from-end-of-list.js
@@ -18,7 +18,32 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    
+    let slow = cur = head;
+    if(n===0) {
+        return head
+    };
+    if(head.next==null) {
+        return null
+    }
+    let m = n;
+    let i = 0;
+    while (m > 0) {
+        fast = cur =cur.next;
+        m--;
+        i++;
+    }
+    if (fast === null) {
+        if (i===n) {
+            return head.next
+        }
+        return null;
+    }
+    while (fast.next !== null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+    slow.next = slow.next.next;
+    return head;
 };
 // @lc code=end
 
