b44285dafcff1aa8bbae4131dcbf5bcbcaa3a86b
yihong <zouzou0208@gmail.com>
2019-11-24 15:55:19 +0800

    day12,34

---
+++ 34.find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=34 lang=javascript
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+  return [nums.indexOf(target), nums.lastIndexOf(target)]
+};
+// @lc code=end
+
