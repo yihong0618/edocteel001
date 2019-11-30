ee758fd9663df386d2f2d764e0d79444caed2d70
yihong <zouzou0208@gmail.com>
2019-11-30 22:51:05 +0800

    add anther method for 66

--- 66.plus-one.js
+++ 66.plus-one.js
@@ -10,15 +10,31 @@
  * @return {number[]}
  */
 const plusOne = function (digits) {
-  for (let i = digits.length - 1; i >= 0; i -= 1) {
-    digits[i] += 1;
-    if (digits[i] === 10) {
-      digits[i] = 0;
-    } else {
-      return digits;
-    }
+  const lastNum = digits.length - 1;
+  if (lastNum === 0) {
+    return digits[lastNum] === 9 ? [1, 0] : [digits[0] + 1];
   }
-  digits.unshift(1);
+  if (digits[lastNum] !== 9) {
+    digits[lastNum] += 1;
+    return digits;
+  }
+  digits.pop();
+  digits = plusOne(digits);
+  digits = digits.concat([0]);
   return digits;
 };
 // @lc code=end
+console.log(plusOne([0]));
+
+// const plusOne = function (digits) {
+//     for (let i = digits.length - 1; i >= 0; i -= 1) {
+//       digits[i] += 1;
+//       if (digits[i] === 10) {
+//         digits[i] = 0;
+//       } else {
+//         return digits;
+//       }
+//     }
+//     digits.unshift(1);
+//     return digits;
+//   };
