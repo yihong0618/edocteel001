51ee2f02cb3ca9d18ec1032354e67de6a5d265b8
yihong0618 <zouzou0208@gmail.com>
2019-12-24 09:19:56 +0800

    day35 ,938

---
+++ 938.range-sum-of-bst.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=938 lang=javascript
+ *
+ * [938] Range Sum of BST
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+const rangeSumBST = function (root, L, R) {
+  sum = 0;
+  if (root === null) {
+    return sum;
+  }
+  if (root.val > L) {
+    sum += rangeSumBST(root.left, L, R);
+  }
+  if (root.val <= R && root.val >= L) {
+    sum += root.val;
+  }
+  if (root.val < R) {
+    sum += rangeSumBST(root.right, L, R);
+  }
+  return sum
+};
+// @lc code=end
